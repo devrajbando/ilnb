f3b7f1e57bd5998baba534aa78e34b3a6e038433
Devraj <devrajbando@gmail.com>
2025-04-05 13:20:54 +0530

    app.jsx issue fixed

---
+++ backend/src/app.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import { createServer } from 'node:http';
+import userRouter from './routes/user.routes.js';
+
+
+
+const app=express()
+
+app.use(cors(
+  {
+    origin:'http://localhost:5173',
+    
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials:true,
+}))
+
+
+
+
+app.use(express.json({
+    limit:'16kb'
+}))
+
+
+
+app.use(express.urlencoded({extended:true,limit:'16kb'}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+app.use('/api/users', userRouter);
+  
+
+
+
+export {app}
