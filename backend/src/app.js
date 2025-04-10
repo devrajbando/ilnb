d84c27877c3bd6657b477ed3396ef05c9793b1aa
Devraj <devrajbando@gmail.com>
2025-04-10 15:00:25 +0530

    .

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ import { createServer } from 'node:http';
 const app=express()
 app.use(cors(
   {
-    origin:['http://localhost:5173','https://investify-mu.vercel.app/'],
+    origin:['http://localhost:5173','https://investify-mu.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
     credentials:true,
